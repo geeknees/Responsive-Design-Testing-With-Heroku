dfc633412351bafd51f306937508a4a40d47d3da
mkawasaki <mkawasaki@sbihg.co.jp>
2016-04-13 10:41:44 +0900

    update

--- public/responsive.js
+++ public/responsive.js
@@ -1,4 +1,4 @@
-var defaultURL = 'mattkersley.com'; //<---- CHANGE TO YOUR WEBSITE URL
+var defaultURL = 'www.google.com'; //<---- CHANGE TO YOUR WEBSITE URL
 
 //show loading graphic
 function showLoader(id) {
