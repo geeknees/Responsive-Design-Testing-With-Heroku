9b9cfa0541a6a74d1a8d7b2d9ccd34a166c61f24
mkawasaki <mkawasaki@sbihg.co.jp>
2016-10-30 20:36:55 +0900

    update

--- server.js
+++ server.js
@@ -10,13 +10,6 @@ app.set('port', process.env.PORT || 3000);
 if (user && pass) {
   app.use(express.basicAuth(user, pass));
 }
-app.use (function (req, res, next) {
-  if (req.secure) {
-    res.redirect('http://' + req.headers.host + req.url);
-  } else {
-    next();
-  }
-});
 
 app.use(express.logger('dev'));
 app.use(express.compress());
@@ -25,3 +18,10 @@ app.use(express.static(__dirname + '/public'));
 app.listen(app.get('port'), function() {
   console.log('Server listening on port %s', app.get('port'));
 });
+
+app.get('*',function(req,res,next){
+  if(req.headers['x-forwarded-proto']!='https')
+    next();
+  else
+    res.redirect('http://' + req.headers.host + req.url);
+})
